1559c88d528760d4311c37207a3cd6eb3b512463
IT Guy <nitinkrajesh@gmail.com>
2022-06-10 12:23:12 +0530

    Flask App - Initial Commit
    Takes in a claim and tokenizes word.
    The keywords are returned with the claim

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FC11-404
 
 This system intends to automate and simplify the fact checking process
+
+Flask API - TO process claims from frontend and return response
---
+++ fact_flask.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from fct import check_mate
+import json
+
+from nltk_trial import extract_keywords
+
+app = Flask(__name__)
+
+@app.route('/checktool',methods=['GET'])
+def checktool():
+    claim1 = request.args.get('claim1')
+
+    keywords = extract_keywords(claim1)
+    
+    return {"Claim":claim1,"Keys":keywords}
+
+if __name__ == '__main__':
+    cm = check_mate()
+    app.run()
+
