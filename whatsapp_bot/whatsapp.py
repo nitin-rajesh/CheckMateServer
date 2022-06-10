04e0777182e2aab968cf1ed29684e4e14b291b55
aravind-02 <aravind020201@gmail.com>
2022-06-10 17:07:39 +0530

    Changes to Whatsapp_bot

--- whatsapp_bot/whatsapp.py
+++ whatsapp_bot/whatsapp.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
+
 app = Flask(__name__)
 
 
@@ -22,31 +23,11 @@ def bot():
     print(incoming_msg)
     print(result['truth'])
     
-    # responded = False
-    # # if 'quote' in incoming_msg:
-    # #     # return a quote
-    # #     r = requests.get('https://api.quotable.io/random')
-    # #     if r.status_code == 200:
-    # #         data = r.json()
-    # #         quote = f'{data["content"]} ({data["author"]})'
-    # #     else:
-    # #         quote = 'I could not retrieve a quote at this time, sorry.'
-    # #     msg.body(quote)
-    # #     responded = True
-    # # if 'cat' in incoming_msg:
-    # #     # return a cat pic
-    # #     msg.media('https://cataas.com/cat')
-    # #     responded = True
-    
-    # msg.body('This claim appears to be '+str(result['truth'].lower()+'!'))
     try:
         msg.body('This claim appears to be '+str(result['truth'].lower()+'!\n*More info:'+str(result['url'].lower())))
     except:
         msg.body('This claim appears to be '+str(result['truth'].lower()+'!')) 
 
-
-    
-    # pwt.sendwhatmsg("+917676265272","welcome to pywhatkit",20,21)
     return str(resp)
 
 
