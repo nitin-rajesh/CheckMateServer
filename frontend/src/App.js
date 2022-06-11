da748d01e3c8d1ac4fd046447aa908c4938e8557
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-06-11 09:52:24 +0530

    Final Commit
    React App with Payments integrated.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,15 +13,10 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             <Link className="navbar-brand" to={'/'}>
-                Codezup
+                Check Mate
             </Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/login'}>
-                    Login
-                  </Link>
-                </li>
               </ul>
             </div>
           </div>
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,102 +1,115 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 function Dashboard() {
-	function loadScript(src) {
-        return new Promise((resolve) => {
-            const script = document.createElement("script");
-            script.src = src;
-            script.onload = () => {
-                resolve(true);
-            };
-            script.onerror = () => {
-                resolve(false);
-            };
-            document.body.appendChild(script);
-        });
-}
+  function loadScript(src) {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  }
 
-	async function displayRazorpay() {
-        const res = await loadScript(
-            "https://checkout.razorpay.com/v1/checkout.js"
-        );
+  async function displayRazorpay() {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
 
-        if (!res) {
-            alert("Razorpay SDK failed to load. Are you online?");
-            return;
-        }
+    if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
 
-        // creating a new order
-        const result = await axios.post("http://localhost:5000/payment/orders");
+    // creating a new order
+    const result = await axios.post("http://localhost:3000/orders");
 
-        if (!result) {
-            alert("Server error. Are you online?");
-            return;
-        }
+    if (!result) {
+      alert("Server error. Are you online?");
+      return;
+    }
 
-        // Getting the order details back
-        const { amount, id: order_id, currency } = result.data;
+    // Getting the order details back
+    const { amount, id: order_id, currency } = result.data;
 
-        const options = {
-            key: "rzp_test_MtpbtcE1dAWxKC", // Enter the Key ID generated from the Dashboard
-            amount: amount.toString(),
-            currency: currency,
-            name: "Check Mate",
-            description: "API key and per 100 calls",
-            image: {},
-            order_id: order_id,
-            handler: async function (response) {
-                const data = {
-                    orderCreationId: order_id,
-                    razorpayPaymentId: response.razorpay_payment_id,
-                    razorpayOrderId: response.razorpay_order_id,
-                    razorpaySignature: response.razorpay_signature,
-                };
-
-                const result = await axios.post("http://localhost:5000/payment/success", data);
-
-                alert(result.data.msg);
-            },
-            prefill: {
-                name: "Check Mate",
-                email: "CheckMate@example.com",
-                contact: "9999999999",
-            },
-            notes: {
-                address: "Bengaluru",
-            },
-            theme: {
-                color: "#61dafb",
-            },
+    const options = {
+      key: "rzp_test_MtpbtcE1dAWxKC", // Enter the Key ID generated from the Dashboard
+      amount: "80",
+      currency: "INR",
+      name: "Check Mate",
+      description: "API key and per 100 calls",
+      image: {},
+      order_id: 2,
+      handler: async function (response) {
+        const data = {
+          orderCreationId: 2,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
         };
 
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
-}
+        // const result = await axios.post(
+        //   "http://localhost:5000/payment/success",
+        //   data
+        // );
+
+        // alert(result.data.msg);
+      },
+      prefill: {
+        name: "Check Mate",
+        email: "CheckMate@example.com",
+        contact: "9999999999",
+      },
+      notes: {
+        address: "Bengaluru",
+      },
+      theme: {
+        color: "#61dafb",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
   return (
-	<div className="auth-wrapper">
-	<div className="auth-inner">
-	<div>
-		<table>
-			<thead>
-				<tr>
-					<th>API</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>isbfoubfobsofbsobfowbfowbf</td>
-					<td><button>
-						Pay
-						</button></td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-		</div>
-		</div>
-  )
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+        <div>
+        <table class="tg">
+<thead>
+  <tr>
+    <th class="tg-c3ow">API_KEY</th>
+    <th class="tg-c3ow">Status</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-c3ow">b804b707c355e0a3d86dfa24f34658b4c20298ee</td>
+    <td class="tg-c3ow"><button><a href="https://rzp.io/i/DWTWY3ZA">Pay</a></button></td>
+  </tr>
+  <tr>
+    <td class="tg-c3ow">d08d3ce2d0834b99489affd1aea3adb3edad9354</td>
+    <td class="tg-c3ow"><button>Active</button></td>
+  </tr>
+  <tr>
+    <td class="tg-c3ow">0958520a4c92c1ca3f2b0df4dead01cd07d16f3a</td>
+    <td class="tg-c3ow"><button>Active</button></td>
+  </tr>
+  <tr>
+    <td class="tg-c3ow">c9399e613f1ca616b0ef84e0569a4755bd644ddb</td>
+    <td class="tg-c3ow"><button><a href="https://rzp.io/i/1mRxPQG4e">Pay</a></button></td>
+  </tr>
+</tbody>
+</table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
